5abdddcc209ac1c0eb0632e1c7a0b9d9a4be9172
glock18 <wnaumov@gmail.com>
2025-07-03 14:35:35 +0100

    bump sdk version

--- Sources/ChaportSDK.swift
+++ Sources/ChaportSDK.swift
@@ -11,7 +11,7 @@ public enum ChaportSDKError: Error {
 
 public class ChaportSDK: NSObject {
     @MainActor public static let shared = ChaportSDK()
-    public static let version = "1.0.25"
+    public static let version = "1.0.26"
     internal weak var delegate: ChaportSDKDelegate?
     internal weak var swiftDelegate: ChaportSDKSwiftDelegate?
     private var _isSessionStarted: Bool = false
